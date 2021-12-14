e9ce405a2db1e0ae2759f46f22b81d39bdb2ecf1
Евгений Ташогло <tashoglo00@mail.ru>
2021-12-14 11:18:19 +0200

    Добавьте файлы проекта.

---
+++ Tashoglo5.1HighVariant-15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tashoglo5._1HighVariant_15
+{
+    class Program
+    {
+        static void Main()
+        {
+            Reverse(1234);
+            Console.ReadKey();
+        }
+        static void Reverse(int number)
+        {
+            int newNum = number % 10;
+            Console.Write(newNum);
+            number /= 10;
+            if (number > 0)
+                Reverse(number);
+        }
+    }
+}
